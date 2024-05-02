befc9941a37ce5892cc466f110b56bbe3e869af3
Iuliia Berezianska <iuliia.berezianska@burberry.com>
2024-05-02 17:50:19 +0200

    Login with auth.json file

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,11 +1,14 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import LoginPage from '../pages/LoginPage';
-import { log } from 'console';
-import { encode } from 'querystring';
+//import { log } from 'console';
+//import { encode } from 'querystring';
 import { encrypt, decrypt } from '../utils/CryptojsUtil';
 import { encryptEnvFile } from '../utils/EncryptEnvFile';
+import logger from '../utils/LoggerUtil';
 
-test('Test login', async ({page}) =>{
+const authFile = 'src/config/auth.json'
+
+test.skip('Test login', async ({page}) =>{
     const loginPage = new LoginPage(page);
     await loginPage.navigateToLoginPage();
     const login = process.env.userId!;
@@ -18,14 +21,25 @@ test('Test login', async ({page}) =>{
     await loginPage.enterPassword(decrypt(process.env.password!));
     const homePage = await loginPage.clickEnterBtn();
     await homePage.expectServiceTitleToBeVisible();
+    logger.info('Test for login is completed');
+    await page.context().storageState({path: authFile});
 })
 
 test.skip('Sample env test', async ({page}) => {
-    //const plainText = 'Hello Iullia!';
-    //const encryptedText = encrypt(plainText);
-    //console.log('SALT:', process.env.SALT);
-    //console.log('Encrypted:', encryptedText);
-    //const decreptedText = decrypt(encryptedText);
-    //console.log('Decrypted:', decreptedText);
+    const plainText = 'Hello Iullia!';
+    const encryptedText = encrypt(plainText);
+    console.log('SALT:', process.env.SALT);
+    console.log('Encrypted:', encryptedText);
+    const decreptedText = decrypt(encryptedText);
+    console.log('Decrypted:', decreptedText);
     encryptEnvFile();
 });
+
+test('Login with auth file', async ({browser}) => {
+    const context = await browser.newContext({storageState: authFile});
+    const page = await context.newPage();
+    await page.goto('https://dwu000003pipz2aq-dev-ed.develop.lightning.force.com/lightning/page/home');
+    await expect(page.getByTitle('Service'),
+    'Service page shoul be visible'
+).toBeVisible();
+})
